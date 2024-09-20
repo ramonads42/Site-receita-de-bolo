350886997c5fdffe59859de78228aa3ec47d54ec
ramonads42 <51218620+ramonads42@users.noreply.github.com>
2024-09-20 09:11:56 -0300

    Delete Site-receita-de-bolo-main directory

--- Site-receita-de-bolo-main/scripts/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const printBtn = document.getElementById('print');
-    printBtn.addEventListener('click', function() {
-        window.print();
-    });
-});
